6e26875ec01c783f89ffc32d9ad3edb11baf0b6a
Nihad Kadić <106343121+KadicNihad@users.noreply.github.com>
2024-12-18 20:20:46 +0100

    Navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import {Navbar, Nav, Container} from 'react-bootstrap';
 
-function NavBar() {
+const NavBar = () => {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">MyWebsite</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
-                        <NavDropdown title="Services" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#service1">Service 1</NavDropdown.Item>
-                            <NavDropdown.Item href="#service2">Service 2</NavDropdown.Item>
-                            <NavDropdown.Item href="#service3">Service 3</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#contact">Contact Us</NavDropdown.Item>
-                        </NavDropdown>
+        <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+            <Container fluid className='text-center'>
+                <Navbar.Brand href="#home">MyBrand</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-collapse"/>
+                <Navbar.Collapse id="navbar-collapse">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#features" className="text-light">
+                            Tasks
+                        </Nav.Link>
+                        <Nav.Link href="#pricing" className="text-light">
+                            Account
+                        </Nav.Link>
                     </Nav>
-                    <Nav>
-                        <Nav.Link href="#login">Login</Nav.Link>
-                        <Nav.Link href="#signup">Sign Up</Nav.Link>
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#logout" className="text-light">
+                            Logout
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
-}
+};
 
 export default NavBar;
