56cb168cb10014aba352077e774ce7fa7c6a3af0
Nihad Kadić <106343121+KadicNihad@users.noreply.github.com>
2024-12-19 23:35:46 +0100

    included Router for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Tasks from './pages/Tasks.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Account from "./pages/Account.jsx";
+import Login from "./pages/Login.jsx";
+import NavBar from "./components/Navbar.jsx";
 function App() {
 
   return (
-      <>
-            <Tasks />
-      </>
+      <Router>
+            <NavBar />
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/tasks" element={<Tasks />} />
+              <Route path="/account" element={<Account />} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+      </Router>
   )
 }
 
