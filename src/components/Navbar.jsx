84d97778821a940204c17fee19faf3c5c62246c2
Nihad Kadić <106343121+KadicNihad@users.noreply.github.com>
2024-12-19 23:37:17 +0100

    added Link from react-router-dom for routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,23 @@
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
-            <Container fluid className='text-center'>
-                <Navbar.Brand href="#home">MyBrand</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbar-collapse"/>
+            <Container fluid className="text-center">
+                <Navbar.Brand as={Link} to="/">MyBrand</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-collapse" />
                 <Navbar.Collapse id="navbar-collapse">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#tasks" className="text-light">
+                        <Nav.Link as={Link} to="/tasks" className="text-light">
                             Tasks
                         </Nav.Link>
-                        <Nav.Link href="#account" className="text-light">
+                        <Nav.Link as={Link} to="/account" className="text-light">
                             Account
                         </Nav.Link>
                     </Nav>
                     <Nav className="ms-auto">
-                        <Nav.Link href="#logout" className="text-light">
+                        <Nav.Link as={Link} to="/login" className="text-light">
                             Logout
                         </Nav.Link>
                     </Nav>
