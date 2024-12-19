8c399db8a20e2f460594c68a06e45a8a2bdd51f1
KadicNihad <nihadkadic95@gmail.com>
2024-12-19 10:38:12 +0100

    insert React Hook on Form, checking value from ls

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,79 @@
 import {Container, Row, Col, Card, FloatingLabel, Form, Button} from 'react-bootstrap';
-function Login () {
-    return(
-    <Container className='mt-5'>
-        <Row  className='justify-content-center mt-5'>
-            <Col xs={12} xl={8} className='mt-5'>
-                <Card >
-                    <Card.Body>
-                        <FloatingLabel
-                            controlId="floatingInput"
-                            label="Email address"
-                            className="mb-3 ">
-                            <Form.Control type="email" placeholder="Email" />
-                        </FloatingLabel>
-                        <FloatingLabel controlId="floatingPassword" label="Password" className='mb-3'>
-                            <Form.Control type="password" placeholder="Password" />
-                        </FloatingLabel>
-                        <Button variant='primary' className='form-control'>Log In </Button>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
-    </Container>
+import {useForm} from 'react-hook-form';
+
+function Login() {
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
+
+    const onSubmit = (data) => {
+
+
+
+        const storedEmail = localStorage.getItem('email');
+        const storedPassword = localStorage.getItem('passwrod');
+
+
+        if(data.email === storedEmail && data.password === storedPassword){
+            alert('Podaci su tacni');
+        }else {
+            alert('Podaci nisu tacni');
+        }
+
+    }
+
+    return (
+        <Container className='mt-5'>
+            <Row className='justify-content-center mt-5'>
+                <Col xs={12} xl={8} className='mt-5'>
+                    <Card>
+                        <Card.Body>
+                            <Form onSubmit={handleSubmit(onSubmit)}>
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    label="Email address"
+                                    className="mb3">
+
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="Email"
+                                        {...register("email", {
+                                            required: "Email je obavezan",
+                                            pattern: {
+                                                value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
+                                                message: "Unesite validan email",
+                                            },
+                                        })}
+                                    />
+                                    {errors.email && (
+                                        <p className="text-danger">{errors.email.message}</p>
+                                    )}
+                                </FloatingLabel>
+                                <FloatingLabel controlId="floatingPassword" label="Password" className='mb-3'>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Password"
+                                        {...register('password', {
+                                            required:'Unesite lozinku',
+                                            minLength: {
+                                                value:6,
+                                                message: 'Password mora imate minimalno 6 karaktera',
+                                        },
+                                        })}
+                                    />
+                                    {errors.password && (
+                                        <p className='text-danger'>{errors.password.message}</p>
+                                    )}
+                                </FloatingLabel>
+                                <Button variant='primary' type='submit' className='form-control'>Log In </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
