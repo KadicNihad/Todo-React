a1a85f5d72a5bc4848898267f5beb73a2b0247f4
KadicNihad <nihadkadic95@gmail.com>
2024-12-20 08:53:25 +0100

    bg and text color changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+        <Navbar bg="info" variant="info" expand="lg" className="px-3">
             <Container fluid className="text-center">
                 <Navbar.Brand as={Link} to="/tasks">MyBrand</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbar-collapse" />
                 <Navbar.Collapse id="navbar-collapse">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to="/tasks" className="text-light">
+                        <Nav.Link as={Link} to="/tasks" className="text-dark">
                             Tasks
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/account" className="text-light">
+                        <Nav.Link as={Link} to="/account" className="text-dark">
                             Account
                         </Nav.Link>
                     </Nav>
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to="/login" className="text-light">
+                        <Nav.Link as={Link} to="/login" className="text-dark">
                             Logout
                         </Nav.Link>
                     </Nav>
